5f1a0cae27088bfd947569f7b0b4f55320d2a103
Vladislav <Fommkaa2304@mail.ru>
2021-10-09 21:48:33 +0300

    Added Spring Security config

---
+++ src/main/java/com/dimevision/orkis/webapp/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.dimevision.orkis.webapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+/**
+ *
+ *
+ * @author Dimevision
+ * @version 0.1
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers().hasAnyRole()
+    }
+}
