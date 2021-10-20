8d9794b85f3772148d966fe8a56d1a005c887ce5
Vladislav <Fommkaa2304@mail.ru>
2021-10-20 18:02:19 +0300

    Added new methods and queries to repositories

--- src/main/java/com/dimevision/orkis/webapp/repository/ClientRepository.java
+++ src/main/java/com/dimevision/orkis/webapp/repository/ClientRepository.java
@@ -2,8 +2,10 @@ package com.dimevision.orkis.webapp.repository;
 
 import com.dimevision.orkis.webapp.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,4 +17,12 @@ import java.util.Optional;
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
     Optional<Client> findClientByEmail(String email);
+
+    @Override
+    @Query(value = "SELECT " +
+            "* " +
+            "FROM client AS c " +
+            "LEFT JOIN passport AS p " +
+            "ON c.passport_id = p.id", nativeQuery = true)
+    List<Client> findAll();
 }
--- src/main/java/com/dimevision/orkis/webapp/repository/EmployeeRepository.java
+++ src/main/java/com/dimevision/orkis/webapp/repository/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.dimevision.orkis.webapp.repository;
 
 import com.dimevision.orkis.webapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,4 +17,13 @@ import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findEmployeeByEmail(String email);
+
+    @Override
+    @Query(value = "SELECT " +
+//            "emp.id, emp.fullname, emp.email, org.name " +
+            "* " +
+            "FROM employee AS emp " +
+            "LEFT JOIN organization AS org " +
+            "ON emp.organization_id = org.id", nativeQuery = true)
+    List<Employee> findAll();
 }
