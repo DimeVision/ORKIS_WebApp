b61cdff15718c28792cedb890f993114144cdd45
Vladislav <Fommkaa2304@mail.ru>
2021-10-26 08:25:09 +0300

    Updated SecurityConfig class

---
+++ src/main/java/com/dimevision/orkis/webapp/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.dimevision.orkis.webapp.config;
+
+import com.dimevision.orkis.webapp.entity.management.Permission;
+import com.dimevision.orkis.webapp.entity.management.Role;
+import com.dimevision.orkis.webapp.service.EmployeeDetailsServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Dimevision
+ * @version 0.1
+ */
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final EmployeeDetailsServiceImplementation employeeService;
+
+    @Autowired
+    public SecurityConfig(EmployeeDetailsServiceImplementation employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/authenticated/**").authenticated()
+                    .antMatchers("/profile/**").authenticated()
+//                .antMatchers("/employees").hasRole(Role.ADMIN.name())
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/employees", true)
+                    .passwordParameter("password")
+                    .usernameParameter("username")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(employeeService);
+
+        return authenticationProvider;
+    }
+}
